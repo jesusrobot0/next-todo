d41363128a55c2509f5ac86863da546d84a72e08
jesusrobot0 <jesusrobot9000@icloud.com>
2024-05-28 23:42:03 -0500

    Add DB configuration

---
+++ src/app/api/hello/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse, NextRequest } from "next/server";
+
+export async function GET(request: Request) {
+  return NextResponse.json({
+    hello: "world",
+    method: "GET",
+  });
+}
+
+export async function POST(request: Request) {
+  return NextResponse.json({
+    hello: "world",
+    method: "POST",
+  });
+}
