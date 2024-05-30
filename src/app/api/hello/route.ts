3211a25a958b2759d1ac295c5e6e791a6f5d685c
jesusrobot0 <jesusrobot9000@icloud.com>
2024-05-30 08:43:54 -0500

    Add GET endpoint for fetching todos with pagination

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -3,13 +3,5 @@ import { NextResponse, NextRequest } from "next/server";
 export async function GET(request: Request) {
   return NextResponse.json({
     hello: "world",
-    method: "GET",
-  });
-}
-
-export async function POST(request: Request) {
-  return NextResponse.json({
-    hello: "world",
-    method: "POST",
   });
 }
---
+++ src/app/api/todos/route.ts
@@ -0,0 +1,25 @@
+import { NextResponse, NextRequest } from "next/server";
+import prisma from "@/lib/prisma";
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const skip = Number(searchParams.get("skip")) ?? 0;
+  const take = Number(searchParams.get("take")) ?? 5;
+
+  if (isNaN(skip)) {
+    return NextResponse.json(
+      { message: "Skip tiene que ser un numero" },
+      { status: 400 }
+    );
+  }
+
+  if (isNaN(take)) {
+    return NextResponse.json(
+      { message: "Take tiene que ser un numero" },
+      { status: 400 }
+    );
+  }
+
+  const todos = await prisma.todo.findMany({ skip, take });
+  return NextResponse.json(todos);
+}
