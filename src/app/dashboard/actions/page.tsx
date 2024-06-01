72397d313dc77ed9e66a129d605b3b7dc49976f2
jesusrobot0 <jesusrobot9000@icloud.com>
2024-06-01 13:02:08 -0500

    Set up directory-based routing for dashboard pages in Next.js

---
+++ src/app/dashboard/actions/page.tsx
@@ -0,0 +1,7 @@
+export default function ActionsPage() {
+  return (
+    <div>
+      <h1>Server Actions Page</h1>
+    </div>
+  );
+}
---
+++ src/app/dashboard/rest/page.tsx
@@ -0,0 +1,7 @@
+export default function RestPage() {
+  return (
+    <div>
+      <h1>Rest Page</h1>
+    </div>
+  );
+}
